21b322f493ce8912717517a97e423f43c8afe56f
Baptiste Lagarde <baptiste.lagarde@centraliens.net>
2014-01-24 07:24:18 +1100

    Let's see how this goes

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+VERSION = '0'
+
+setup(
+    name = 'travistest',
+    version = VERSION,
+    install_requires = 'unicodecsv>=0.9.4'
+)
---
+++ test.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+from __future__ import print_function
+import unicodecsv
+from unicodecsv import DictReader
+
+
+print('yay')
