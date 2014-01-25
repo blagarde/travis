73ce1d7804c6d63ca6e67106b006b60f7b25e210
Baptiste Lagarde <baptiste.lagarde@centraliens.net>
2014-01-26 10:29:58 +1100

    ADD: DictReader test

--- test.py
+++ test.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python
 from __future__ import print_function
+from __future__ import unicode_literals
 import unicodecsv
 from unicodecsv import DictReader
-
-
+import sys
+print(sys.version)
+try:
+    import StringIO as io
+except ImportError:
+    pass  # Python >= 3
+from io import StringIO
+d = DictReader(StringIO('a,b,c\n1,2,3'))
 print('yay')
